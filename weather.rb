586ec22210108529dbc270ba5a4ad743a9f120eb
aumdove <aumdove13@gmail.com>
2016-12-22 16:58:53 -1000

    adding line breaks

--- weather.rb
+++ weather.rb
@@ -6,9 +6,9 @@ place = gets.chomp
 barometer = Barometer.new(place)
 weather = barometer.measure
 
-puts "Today's Weather for: "+ place.upcase
-puts "Current temperature: " + weather.current.temperature.f + " degrees F"
-puts "Conditions: " + weather.current.condition
+puts "Today's Weather for: "+ place.upcase + "\n"
+puts "Current temperature: " + weather.current.temperature.f + " degrees F" + "\n"
+puts "Conditions: " + weather.current.condition + "\n"
 
 tomorrow = Time.now.strftime('%d').to_i + 1
 
@@ -18,8 +18,9 @@ weather.forecast.each do |forecast|
     if day == tomorrow
         dayName = "TOMORROW"
     else
-        dayName = forecast.starts_at.strftime('%A')
+        dayName = forecast.starts_at.strftime('%A').upcase
     end
 
-    puts dayName + ' is going to be ' + forecast.icon + ' with lows around ' + forecast.low.f.to_s + ' and highs around ' + forecast.high.f.to_s
+    puts dayName + ' is going to be ' + forecast.icon + ' with lows around ' + forecast.low.f.to_s + ' and highs around ' + forecast.high.f.to_s + "\n"
+    puts "\n"
 end
